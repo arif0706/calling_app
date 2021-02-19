af08f955fe1f263656248624c48d4eb62c039526
arif0706 <56088082+arif0706@users.noreply.github.com>
2021-02-19 21:25:46 +0530

    Update FCM service.java

--- app/src/main/java/com/example/FCMService.java
+++ app/src/main/java/com/example/FCMService.java
@@ -8,7 +8,7 @@ import retrofit2.http.POST;
 public interface FCMService {
     @Headers({
             "Content-Type:application/json",
-            "Authorization:key=AAAA8NeVvkk:APA91bGZJlCzyEFr8_ji5DMU6qmRIjKC7d8i64IgXWPlS3jBSEnrtL2Gtth-notRuz80jx14kV_0nChASYLqpb17tzTVsBzfcxfvdDfHqH3HHEI5b696vpt_9xJuisIzK_DasQjJ9e-A"
+            "Authorization:key=<Authorization_key>"
 
     })
     @POST("fcm/send")
